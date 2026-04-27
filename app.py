f2f410a9b2cb651f760dd4cc1840d747d0db78b4
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-27 19:23:09 -0300

    adding login route

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.user import User
 from models.meal import Meal
 from database import db
+from flask_login import LoginManager, login_user
+import bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/daily-diet-api'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User.query.filter_by(username=username)
+
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
+            login_user(user)
+            return jsonify({"message": "Autenticação realizada com sucesso!"})
+        
+    return jsonify({"message": "Credenciais inválidas!"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
