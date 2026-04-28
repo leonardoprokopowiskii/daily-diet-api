007edb1ef080accc5bce56325fe1e06920a1e8a7
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-27 22:30:46 -0300

    improving profile management

--- app.py
+++ app.py
@@ -105,9 +105,10 @@ def create_meal():
     meal_name = data.get('name')
     meal_description = data.get('description')
     date_meal = data.get('date_meal')
+    on_diet = data.get('on_diet')
 
     if meal_name and meal_description and date_meal:
-        meal = Meal(name=meal_name, description=meal_description, date_meal=date_meal, user_id=current_user.id)
+        meal = Meal(name=meal_name, description=meal_description, date_meal=date_meal, user_id=current_user.id, on_diet=on_diet)
         db.session.add(meal)
         db.session.commit()
         return jsonify({"message": "Refeição adicionada com sucesso!"})
@@ -124,7 +125,8 @@ def read_meals():
             "id": meal.id,
             "name": meal.name,
             "description": meal.description,
-            "date": meal.date_meal
+            "date": meal.date_meal,
+            "on_diet": meal.on_diet
         }
         for meal in meals
     ]
@@ -132,17 +134,16 @@ def read_meals():
 @app.route('/meal/<int:meal_id>', methods=['GET'])
 @login_required
 def read_meal(meal_id):
-    meal = Meal.query.get(meal_id)
+    meal = Meal.query.filter_by(user_id=current_user.id, id=meal_id).first()
 
     if meal:
-        return [
-            {
-                "id": meal.id,
-                "name": meal.name,
-                "description": meal.description,
-                "date": meal.date_meal
-            }
-        ]
+        return {
+            "id": meal.id,
+            "name": meal.name,
+            "description": meal.description,
+            "date": meal.date_meal,
+            "on_diet": meal.on_diet
+        }
     
     return jsonify({"message": "Refeição não encontrada!"}), 404
 
@@ -153,12 +154,17 @@ def update_meal(meal_id):
     new_name = data.get('name')
     new_description = data.get('description')
     new_date = data.get('date_meal')
-    meal = Meal.query.get(meal_id)
+    new_on_diet = data.get('on_diet')
+    meal = Meal.query.filter_by(user_id=current_user.id, id=meal_id).first()
+
+    if not meal:
+        return jsonify({"message": "Refeição não encontrada!"}), 404
 
     if new_name and new_description and new_date:
         meal.name = new_name
         meal.description = new_description
         meal.date_meal = new_date
+        meal.on_diet = new_on_diet
         db.session.commit()
         
         return jsonify({"message": "Refeição atualizada com sucesso!"})
@@ -168,7 +174,7 @@ def update_meal(meal_id):
 @app.route('/meal/<int:meal_id>', methods=['DELETE'])
 @login_required
 def delete_meal(meal_id):
-    meal = Meal.query.get(meal_id)
+    meal = Meal.query.filter_by(user_id=current_user.id, id=meal_id).first()
 
     if meal:
         db.session.delete(meal)
