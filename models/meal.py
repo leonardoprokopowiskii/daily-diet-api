f012ea0dc9ab2057de5f59f3473494f79136f0ec
Leonardo Prokopowiski <leoprokopowiskii@gmail.com>
2026-04-27 22:30:08 -0300

    adding on_diet column in meal table

--- models/meal.py
+++ models/meal.py
@@ -4,6 +4,7 @@ class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(80), nullable=False)
+    on_diet = db.Column(db.Boolean, nullable=False, default=True)
     date_meal = db.Column(db.DateTime, nullable=False)
 
     # Adiciona chave estrangeira
